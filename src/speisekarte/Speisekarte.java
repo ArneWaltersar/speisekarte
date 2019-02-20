0cbe76648d9f9954f695956e9c1f6c07ec8083f1
rauerjakob <rauerjakob@R166PC16.schule.local>
2019-02-20 14:29:19 +0100

    havent adjusted files yet :(

---
+++ src/speisekarte/Speisekarte.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package speisekarte;
+
+/**
+ *https://pastebin.com/bd0kGnXe
+ * 
+ * @author rauerjakob
+ */
+public class Speisekarte {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
